fc6f2d35fa01205614e299423251744faf424e69
Ramesh <ramesh.ks1272@gmail.com>
2026-07-28 20:12:25 +0530

    fix: move api endpoint to root directory for Vercel functions

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { EvCursor } from "./components/shared.jsx";
 import Store from "./features/store/Store.jsx";
 import Admin from "./features/admin/Admin.jsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
-  <>
-    <EvCursor />
-    {location.pathname.startsWith("/admin") ? <Admin /> : <Store />}
-  </>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <EvCursor />
+      <Routes>
+        <Route path="/admin/*" element={<Admin />} />
+        <Route path="/*" element={<Store />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
